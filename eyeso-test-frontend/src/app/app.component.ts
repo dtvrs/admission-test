fea201c6b2c83dc58e8104708f33e8a27de95945
David Tavares <tavares.david.gil@gmail.com>
2018-09-18 20:32:07 +0100

    Corrects Responsive Menu; Minor styling corrections;

--- eyeso-test-frontend/src/app/app.component.ts
+++ eyeso-test-frontend/src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+
+const TASK_KEY = '49d07488-3d51-5967-9746-8e3c469e9055';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  public navBarCollapsed = false
+  
+  @Input() public totalFinishedTasks:number = 0;
+}
